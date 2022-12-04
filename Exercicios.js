c0822384cd924d5b09291d85ef408a80856da50a
Guvilla15 <117367794+Guvilla15@users.noreply.github.com>
2022-12-04 14:56:58 -0300

    Add files via upload

---
+++ Exercicios.js
@@ -0,0 +1,35 @@
+//let weight
+
+//console.log(typeof weight)
+
+/*let name = "Gustavo"
+ let age = 16
+ let stars = 5.0
+ let isSubscribed = true*/
+
+// let student = {
+//   name: `Gustavo`,
+//   weight: 82,
+//   age: 16,
+//   stars: 5.0,
+//   issubscribed: true,
+// };
+
+// const studentDois = {
+//   name: `Guilherme`,
+//   weight: 67,
+//   age: 21,
+//   stars: 4.8,
+//   issubscribed: true,
+// };
+
+// // console.log(`${student.name} de idade ${student.age} pesa ${student.weight} KG`);
+
+// let students = [student, studentDois];
+
+// console.log(students);
+
+;
+// Se console.log imprimir a var a vai ser undefined porque o html lê de cima pra baixo ou seja a var a não foi criada antes pois não foi chamada antes.
+
+
