86f5041b95c98b18c703a057efd6e723a911c923
Guvilla15 <117367794+Guvilla15@users.noreply.github.com>
2022-12-07 14:32:25 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,6 @@
+
+function creatPhrases () {
+    console.log(`Aula sobre functions`)
+    console.log(`A vida é como uma caixinha de surpresa`)
+    console.log(`talento sem esforço não é nada`)    
+}
